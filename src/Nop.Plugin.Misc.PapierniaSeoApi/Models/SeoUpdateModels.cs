a172f29aa8dea4ab64b6fdefa76d520ea38c96cb
pwrobel194-cmd <pwrobel194@gmail.com>
2026-09-24 18:34:06 +0200

    Add src/Nop.Plugin.Misc.PapierniaSeoApi/Models/SeoUpdateModels.cs

---
+++ src/Nop.Plugin.Misc.PapierniaSeoApi/Models/SeoUpdateModels.cs
@@ -0,0 +1,22 @@
+namespace Nop.Plugin.Misc.PapierniaSeoApi.Models;
+
+public class ProductSeoUpdateRequest
+{
+    public string? MetaTitle { get; set; }
+    public string? MetaDescription { get; set; }
+    public string? MetaKeywords { get; set; }
+    public string? ShortDescription { get; set; }
+    public string? FullDescription { get; set; }
+    public string? RequestId { get; set; }
+    public string? Reason { get; set; }
+}
+
+public class CategorySeoUpdateRequest
+{
+    public string? MetaTitle { get; set; }
+    public string? MetaDescription { get; set; }
+    public string? MetaKeywords { get; set; }
+    public string? Description { get; set; }
+    public string? RequestId { get; set; }
+    public string? Reason { get; set; }
+}
